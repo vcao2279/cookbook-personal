915f27a38d75207293d0989d060ce8d6e3af3784
Vu Cao <cdhvu@yahoo.com>
2018-11-12 10:16:11 -0800

    Setup basic frontend and backend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">Hi</div>
+      </Router>
+    );
+  }
+}
+
+export default App;
